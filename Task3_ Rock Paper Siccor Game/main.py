4b0c825c5779eb30c0646b1a426d225fc6009e55
Muhammad Affan Shahnawaz <affan.snz@gmail.com>
2024-11-26 00:27:46 +0500

    Create main.py

---
+++ Task3_ Rock Paper Siccor Game/main.py
@@ -0,0 +1,17 @@
+import random 
+
+def play():
+    user = input("What's your choice? rock(r), paper(p) or scissor(s)\n")
+    computer = random.choice(["r", "p", "s"])
+    if user == computer:
+        return "It's a tie"
+    if is_Win(user, computer):
+        return "You win!"
+    return "You Lost!"
+
+
+def is_Win(player, computer):
+    if (player == "r" and computer == "s") or (player == "p" and computer == "r") or (player == "s" and computer == "p"):
+        return True
+    
+print(play())
